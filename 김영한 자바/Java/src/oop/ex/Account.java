add8bc9cb0e61342ca88b1b8acca0e4fde0f5b62
yunseungbum <ibm15444@naver.com>
2024-09-06 14:27:39 +0900

    자바 클래스 예제
    객체 지향적으로 바꾸기

---
+++ 김영한 자바/Java/src/oop/ex/Account.java
@@ -0,0 +1,18 @@
+package oop.ex;
+
+public class Account {
+	int balance;
+	void deposit(int amount){
+		balance += amount;
+	}
+	
+	void withdraw(int amount) {
+		
+		if(balance >= amount) {
+			balance -= amount;
+		}else {
+			System.out.println("잔액부족");
+		}
+		
+	}
+}
